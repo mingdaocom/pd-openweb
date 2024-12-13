2adf439c145b62d59002b52f420315f376c47abd
beckjin <beck.jin@mingdao.com>
2024-12-13 16:41:36 +0800

    5.8.3

--- src/pages/worksheet/common/ViewConfig/components/ParameterSet/index.jsx
+++ src/pages/worksheet/common/ViewConfig/components/ParameterSet/index.jsx
@@ -107,6 +107,7 @@ const CustomControlDrop = props => {
         <SortColumns
           //关联表的设置 可拖拽排序
           sortAutoChange
+          isShowColumns
           noempty={false} //不需要至少显示一列
           controlsSorts={value}
           showControls={value}
